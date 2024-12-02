64f7930ce9324fbcbd2432d2510faf80ccdc389f
unclejimbob <cantor001@hotmail.com>
2024-12-02 20:52:10 +1000

    Lecure 162

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    
+    <PublicNav v-if="!user" />
+    <UserNav v-else />
   </div>
 </template>
 
 <script lang="ts" setup>
+import PublicNav from "./PublicNav.vue";
 
+const user = useSupabaseUser();
 </script>
 
-<style>
-
-</style>
+<style></style>
