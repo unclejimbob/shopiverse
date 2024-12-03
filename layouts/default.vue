341dbf1d7c1e13324d4c25ad57b47a7ac76a2f90
unclejimbob <cantor001@hotmail.com>
2024-12-03 11:39:08 +1000

    Lecure 163 - forgot supabase keys

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Header />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,26 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/supabase"],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  },
+  app: {
+    head: {
+      title: "Shopiverse",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js",
+          defer: true,
+        },
+      ],
+    },
+  },
 });
