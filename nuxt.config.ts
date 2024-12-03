f4c0ad9b40507290118875b9fd0e9d78e56cff8c
unclejimbob <cantor001@hotmail.com>
2024-12-03 22:01:01 +1000

    Lecture 167

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/supabase"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon", "@nuxtjs/supabase", "@nuxt/eslint"],
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
