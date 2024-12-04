428fb1a87e175ea652bc3852571ed4f76846606d
unclejimbob <cantor001@hotmail.com>
2024-12-04 14:11:33 +1000

    Lecture 170

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <div class="grid grid-cols2 gap-4">
+    <div v-if="products" v-for="product in products">
+      <Product :product="product" />
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const { data: products } = await useFetch("/api/products");
+</script>
 
 <style></style>
